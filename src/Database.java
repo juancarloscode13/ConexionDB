e4edae10fffd0675fc8b4ad1972edae431b10255
juancarloscode13 <jsanmiguelcarmona@gmail.com>
2025-10-16 21:48:54 +0200

    clase 09/10/2025

--- src/Database.java
+++ src/Database.java
@@ -12,12 +12,13 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 
 public class Database {
-    static String c_ruta_padre="";
+    static String c_ruta_padre="C:\\Users\\sanmiguel.cacar\\Desktop\\intellij\\ConexionDB\\src";
     static String c_log_file="//log_data_base";
     static String c_tipo_info="I";
     static String c_tipo_error="E";
     static String c_tipo_aviso="W";
 
+    //Aquí escribimos las operaciones que se realizan en el fichero log.
     private static void escribe_log(BufferedWriter v_log_buf, String v_tipo, String v_traza){
         DateFormat v_fecha_hora_actual=new SimpleDateFormat("yyyyMMdd HH:mm:ss");
         Date v_fecha_actual=new Date();
@@ -34,6 +35,7 @@ public class Database {
         }
     }
 
+    //Establecemos la conexión con la base de datos.
     private static Connection conecta_db(BufferedWriter v_log_buf){
         Connection v_conexion = null;
         escribe_log(v_log_buf, c_tipo_info, "Estableciendo conexión");
@@ -57,20 +59,22 @@ public class Database {
         return v_conexion;
     }
 
+    //Imprimimos los resultados de las consultas que realizamos.
     private static void imprimeInforme(ResultSet i_datos, BufferedWriter v_log_buf) throws SQLException{
         ResultSetMetaData v_rs_metadatos= i_datos.getMetaData();
         int v_num_cols=v_rs_metadatos.getColumnCount();
         escribe_log(v_log_buf, c_tipo_info, "Imprimiendo informe.");
         while(i_datos.next()){
-            for (int i = 0; i <= v_num_cols ; i++) {
-                if (i>1) System.out.println(" | ");
-                System.out.println(i_datos.getString(i));
+            for (int i = 1; i <= v_num_cols ; i++) {
+                if (i>1) System.out.print(" | ");
+                System.out.print(i_datos.getString(i));
             }
             System.out.println("");
         }
         escribe_log(v_log_buf, c_tipo_info, "Informe impreso.");
     }
 
+    //Aquí lanzamos la consulta.
     private static void lanzaConsulta(Connection i_conexion, String i_consulta, BufferedWriter v_log_buf){
         Statement v_sentencia=null;
         ResultSet v_resultado=null;
@@ -86,10 +90,58 @@ public class Database {
             System.out.println("SQLException: "+e.getMessage());
             System.out.println("SQLState: "+e.getSQLState());
             System.out.println("VendorError: "+e.getErrorCode());
+        }finally{
+            if (v_resultado!=null){
+                try {
+                    v_resultado.close();
+                } catch (SQLException e) {}
+                v_resultado=null;
+            }
+
+            if (v_sentencia!=null){
+                try{
+                    v_sentencia.close();
+                } catch (SQLException e) {}
+                v_sentencia=null;
+            }
         }
     }
 
+    //Ejecutamos.
     public static void main(String[] args) throws Exception{
 
+        String v_ruta_log=c_ruta_padre+"//Logs";
+        //Fichero log
+        Date v_fecha=new Date();
+        DateFormat v_fecha_hora=new SimpleDateFormat("yyyyMMdd_HHmmss");
+        FileWriter v_file_log=new FileWriter(v_ruta_log+c_log_file+v_fecha_hora.format(v_fecha)+".log");
+        BufferedWriter v_log_Writer=new BufferedWriter(v_file_log);
+
+        try {
+            System.out.println("Comienza la ejecución");
+            escribe_log(v_log_Writer, c_tipo_info, "Comienza ejecución");
+
+            Connection v_con_db=conecta_db(v_log_Writer);
+            lanzaConsulta(v_con_db, "select Code, Name, Continent from world.country CO limit 5;", v_log_Writer);
+
+            System.out.println("----------------------------------------------------------------------------------");
+            //Paises que hablan español y capital.
+            lanzaConsulta(v_con_db, "select city.name, country.name, cl.language from city city join country country join countrylanguage cl where (city.id=country.capital and cl.countrycode=country.code) and cl.language='Spanish';", v_log_Writer);
+
+            System.out.println("----------------------------------------------------------------------------------");
+            //Los 5 países con mayor número de idiomas.
+            lanzaConsulta(v_con_db, "select co.code, count(language), co.name from world.country co join world.countrylanguage cl on co.code=cl.CountryCode group by co.code order by count(language) desc limit 5;", v_log_Writer);
+
+            System.out.println("----------------------------------------------------------------------------------");
+            //País con mayor número de ciudadanos.
+            lanzaConsulta(v_con_db, "", v_log_Writer);
+
+            System.out.println("Fin de la ejecución");
+            escribe_log(v_log_Writer, c_tipo_info, "Fin de la ejecución");
+        }catch (Exception e){
+            System.out.println("Error en la ejecución");
+        }finally {
+            if (v_log_Writer!=null) v_log_Writer.close();
+        }
     }
 }
